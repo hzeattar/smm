ce442c33197f8617afd26dcddc28e342c9024ab7
hesham talaat <61442482+hzeattar@users.noreply.github.com>
2026-09-08 22:05:27 +0300

    Polish Yellow Duck public header UX

--- overrides/resources/views/web/partials/header.blade.php
+++ overrides/resources/views/web/partials/header.blade.php
@@ -1,44 +1,44 @@
 <!-- ***** Preloader Start ***** -->
-<div id="preloader">
-    <div class="jumper">
-        <div></div><div></div><div></div>
-    </div>
+<div id="preloader" aria-hidden="true">
+    <div class="jumper"><div></div><div></div><div></div></div>
 </div>
 <!-- ***** Preloader End ***** -->
 
-<header class="header-area header-sticky">
+<header class="header-area header-sticky yd-public-header">
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <nav class="main-nav">
-                    <a href="{{route('welcome')}}" class="logo yellow-duck-lockup" aria-label="البطة الصفرا لخدمات السوشيال ميديا">
+                <nav class="main-nav" aria-label="Primary navigation">
+                    <a href="{{route('welcome')}}" class="logo yellow-duck-lockup yd-public-logo" aria-label="البطة الصفرا لخدمات السوشيال ميديا">
                         <img src="{{asset('brand/yellow-duck.svg')}}" alt="البطة الصفرا">
                         <span class="yellow-duck-brand">البطة الصفرا<small>خدمات السوشيال ميديا</small></span>
                     </a>
 
                     <ul class="nav">
-                        <li><a class="link" href="@if($links) {{route('welcome').'#welcome'}}@else{{'#welcome'}}@endif">{{Helper::getLang('Home')}}</a></li>
+                        <li><a class="link active" href="@if($links) {{route('welcome').'#welcome'}}@else{{'#welcome'}}@endif">{{Helper::getLang('Home')}}</a></li>
                         <li><a class="link services" href="@if($links) {{route('welcome').'#services'}}@else{{'#services'}}@endif">{{Helper::getLang('Services')}}</a></li>
                         <li><a class="link" href="@if($links) {{route('welcome').'#features'}}@else{{'#features'}}@endif">{{Helper::getLang('Features')}}</a></li>
                         <li><a class="link" href="@if($links) {{route('welcome').'#testimonials'}}@else{{'#testimonials'}}@endif">{{Helper::getLang('Testimonials')}}</a></li>
                         <li><a class="link" href="@if($links) {{route('welcome').'#contact-us'}}@else{{'#contact-us'}}@endif">{{Helper::getLang('Contact Us')}}</a></li>
+
                         @if (Helper::settings('user_login') === 'on')
                             <li class="login"><a href="{{route('login')}}">{{Helper::getLang('login')}}</a></li>
                         @endif
                         @if (Helper::settings('user_registration') === 'on')
                             <li class="try"><a href="{{route('register')}}">{{Helper::getLang('sign up')}}</a></li>
                         @endif
-                        <li>
+
+                        <li class="yd-language-item">
                             <div class="dropdown">
-                                <div class="dropdown-toggle pt-2" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <img width="30" src="{{asset('images/'.$lang->image)}}" alt="{{$lang->name}}">
-                                    <i class="fas fa-chevron-down"></i>
-                                </div>
+                                <button class="dropdown-toggle yd-language-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" type="button">
+                                    <img src="{{asset('images/'.$lang->image)}}" alt="{{$lang->name}}" width="28" height="28">
+                                    <i class="fas fa-chevron-down" aria-hidden="true"></i>
+                                </button>
                                 <div class="dropdown-menu">
                                     @foreach ($languages as $language)
                                         <a class="dropdown-item" href="{{route('languages.set-language',$language->id)}}">
-                                            <img width="20" src="{{asset('images/'.$language->image)}}" alt="{{$language->name}}">
-                                            {{$language->name}}
+                                            <img src="{{asset('images/'.$language->image)}}" alt="" width="20" height="20">
+                                            <span>{{$language->name}}</span>
                                         </a>
                                     @endforeach
                                 </div>
@@ -46,7 +46,7 @@
                         </li>
                     </ul>
 
-                    <a class="menu-trigger"><span>Menu</span></a>
+                    <a class="menu-trigger" aria-label="Open navigation menu"><span>Menu</span></a>
                 </nav>
             </div>
         </div>
