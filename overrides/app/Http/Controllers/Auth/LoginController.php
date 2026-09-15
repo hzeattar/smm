fd9a97e33d59ed30a0a1804ecdc281e4b3319214
hesham talaat <61442482+hzeattar@users.noreply.github.com>
2026-09-15 16:55:43 +0300

    Prevent cached user login forms with stale CSRF tokens

--- overrides/app/Http/Controllers/Auth/LoginController.php
+++ overrides/app/Http/Controllers/Auth/LoginController.php
@@ -20,6 +20,15 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
+    public function showLoginForm()
+    {
+        return response()
+            ->view('auth.login')
+            ->header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+            ->header('Pragma', 'no-cache')
+            ->header('Expires', '0');
+    }
+
     protected function credentials(Request $request)
     {
         return [
