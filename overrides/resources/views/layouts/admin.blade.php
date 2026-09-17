65fb3030c595a72ba09e4fa0c79871491ba95e14
hesham talaat <61442482+hzeattar@users.noreply.github.com>
2026-09-17 20:55:53 +0300

    Bust manual deposit browser script cache

--- overrides/resources/views/layouts/admin.blade.php
+++ overrides/resources/views/layouts/admin.blade.php
@@ -63,7 +63,7 @@
 </div>
 
 <script src="{{ asset('js/app.js') }}"></script>
-<script src="{{ asset('js/manual-deposit-hardening.js') }}"></script>
+<script src="{{ asset('js/manual-deposit-hardening.js') }}?v=20260917-2042"></script>
 <script>
 (function () {
     function ready(fn){ if(document.readyState !== 'loading'){ fn(); } else { document.addEventListener('DOMContentLoaded', fn); } }
